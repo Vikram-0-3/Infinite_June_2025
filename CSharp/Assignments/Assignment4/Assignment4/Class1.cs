532c1847b333b35d042325282de2977e9393e113
Vikram-0-3 <vikram.rajan@infinite.com>
2025-07-01 14:34:05 +0530

    A4

---
+++ CSharp/Assignments/Assignment4/Assignment4/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+delegate void RingEventHandler();
+
+class MobilePhone
+{
+    public event RingEventHandler OnRing;
+
+    public void ReceiveCall()
+    {
+        if (OnRing != null)
+        {
+            OnRing();
+        }
+    }
+}
+
+class RingtonePlayer
+{
+    public void PlayRingtone()
+    {
+        Console.WriteLine("Playing ringtone...");
+    }
+}
+
+class ScreenDisplay
+{
+    public void ShowCallerInfo()
+    {
+        Console.WriteLine("Displaying caller information...");
+    }
+}
+
+class VibrationMotor
+{
+    public void StartVibrating()
+    {
+        Console.WriteLine("Phone is vibrating...");
+    }
+}
+
+class Program2
+{
+    static void Main()
+    {
+        MobilePhone phone = new MobilePhone();
+
+        RingtonePlayer sound = new RingtonePlayer();
+        ScreenDisplay screen = new ScreenDisplay();
+        VibrationMotor vibe = new VibrationMotor();
+
+        phone.OnRing += sound.PlayRingtone;
+        phone.OnRing += screen.ShowCallerInfo;
+        phone.OnRing += vibe.StartVibrating;
+
+        phone.ReceiveCall();
+    }
+}
---
+++ CSharp/Assignments/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Employee
+{
+    public int Id;
+    public string Name;
+    public string Department;
+    public double Salary;
+}
+
+class Program
+{
+    static List<Employee> Employees = new List<Employee>();
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("===== Employee Management Menu =====");
+            Console.WriteLine("1. Add New Employee");
+            Console.WriteLine("2. View All Employees");
+            Console.WriteLine("3. Search Employee by ID");
+            Console.WriteLine("4. Update Employee Details");
+            Console.WriteLine("5. Delete Employee");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("====================================");
+            Console.Write("Enter your choice: ");
+
+            try
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1) AddEmployee();
+                else if (choice == 2) ViewEmployees();
+                else if (choice == 3) SearchEmployee();
+                else if (choice == 4) UpdateEmployee();
+                else if (choice == 5) DeleteEmployee();
+                else if (choice == 6) break;
+                else Console.WriteLine("Invalid option");
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+        }
+    }
+
+    static void AddEmployee()
+    {
+        Employee emp = new Employee();
+
+        Console.Write("Enter ID: ");
+        emp.Id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Name: ");
+        emp.Name = Console.ReadLine();
+
+        Console.Write("Enter Department: ");
+        emp.Department = Console.ReadLine();
+
+        Console.Write("Enter Salary: ");
+        emp.Salary = Convert.ToDouble(Console.ReadLine());
+
+        Employees.Add(emp);
+        Console.WriteLine("Employee added");
+    }
+
+    static void ViewEmployees()
+    {
+        foreach (Employee emp in Employees)
+        {
+            Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, Dept: {emp.Department}, Salary: {emp.Salary}");
+        }
+    }
+
+    static void SearchEmployee()
+    {
+        Console.Write("Enter ID to search: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Employee emp = Employees.Find(e => e.Id == id);
+
+        if (emp != null)
+            Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, Dept: {emp.Department}, Salary: {emp.Salary}");
+        else
+            Console.WriteLine("Employee not found");
+    }
+
+    static void UpdateEmployee()
+    {
+        Console.Write("Enter ID to update: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Employee emp = Employees.Find(e => e.Id == id);
+
+        if (emp != null)
+        {
+            Console.Write("Enter New Name: ");
+            emp.Name = Console.ReadLine();
+
+            Console.Write("Enter New Department: ");
+            emp.Department = Console.ReadLine();
+
+            Console.Write("Enter New Salary: ");
+            emp.Salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Employee updated");
+        }
+        else
+        {
+            Console.WriteLine("Employee not found");
+        }
+    }
+
+    static void DeleteEmployee()
+    {
+        Console.Write("Enter ID to delete: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Employee emp = Employees.Find(e => e.Id == id);
+
+        if (emp != null)
+        {
+            Employees.Remove(emp);
+            Console.WriteLine("Employee deleted");
+        }
+        else
+        {
+            Console.WriteLine("Employee not found");
+        }
+    }
+}
